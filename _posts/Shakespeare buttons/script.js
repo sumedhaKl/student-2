68d2a9adc884731c4dccc27927694a10bf6f3111
sumedhaKl <sumedhakamaraju@gmail.com>
2023-10-17 02:05:14 -0700

    first commit

--- _posts/Shakespeare buttons/script.js
+++ _posts/Shakespeare buttons/script.js
@@ -2,6 +2,12 @@ const shakespeareanTexts = [
     "To be or not to be, that is the question.",
     "All the world's a stage, and all the men and women merely players.",
     "Love all, trust a few, do wrong to none.",
+    "What's in a name? That which we call a rose by any other name would smell as sweet.",
+    "The better part of Valour, is Discretion.",
+    "Brevity is the soul of wit.",
+    "The lady doth protest too much, methinks.",
+    "To thine own self be true, and it must follow, as the night the day, thou canst not then be false to any man.",
+    "This above all: to thine own self be true.",
     // Add more Shakespearean texts here
 ];
 
