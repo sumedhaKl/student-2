0defe8991a5b29f83d979ce781114cb224234dc3
sumedhaKl <sumedhakamaraju@gmail.com>
2024-02-15 01:19:56 +0000

    first commit

--- _posts/model/main.py
+++ _posts/model/main.py
@@ -2,41 +2,39 @@ from flask import render_template, request
 from flask.cli import AppGroup
 from flask_cors import CORS
 from __init__ import app, db
-from api.user import user_api
-from api.player import player_api
-from model_users import initUsers
-from model_players import initPlayers
-from projects.projects import app_projects
+from users import user_api
+from players import player_api
+from model.users import initUsers
+from model.players import initPlayers
 
-# Enable CORS for your app
-cors = CORS(app)
+CORS(app)
 
-# Register URIs
-app.register_blueprint(user_api)
+# Initialize the SQLAlchemy object to work with the Flask app instance
+db.init_app(app)
+
+# register URIs
+app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
-app.register_blueprint(app_projects)
 
-# Error handler for 404
-@app.errorhandler(404)
+@app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
+    # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
-# Default route
-@app.route('/')
+@app.route('/')  # connects default URL to index() function
 def index():
     return render_template("index.html")
 
-# Table route
-@app.route('/table/')
+@app.route('/table/')  # connects /stub/ URL to stub() function
 def table():
     return render_template("table.html")
 
-# Before each request, set CORS origins
 @app.before_request
 def before_request():
+    # Check if the request came from a specific origin
     allowed_origin = request.headers.get('Origin')
     if allowed_origin in ['http://localhost:4100', 'http://127.0.0.1:4100', 'https://nighthawkcoders.github.io']:
-        CORS(app, origins=allowed_origin)
+        CORS._origins = allowed_origin
 
 # Create an AppGroup for custom commands
 custom_cli = AppGroup('custom', help='Custom commands')
@@ -49,7 +47,8 @@ def generate_data():
 
 # Register the custom command group with the Flask application
 app.cli.add_command(custom_cli)
-
-# Run the application on the development server
+        
+# this runs the application on the development server
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8086)
+    # change name for testing
+    app.run(debug=True, host="0.0.0.0", port="8086")
