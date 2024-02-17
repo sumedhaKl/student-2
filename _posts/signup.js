7f45125ac0ee4cb94c0572692b5376204938a5fc
sumedhaKl <sumedhakamaraju@gmail.com>
2024-02-16 18:00:12 -0800

    first commit

--- _posts/signup.js
+++ _posts/signup.js
@@ -3,7 +3,17 @@ function signup(event) {
 
     const newUsername = document.getElementById("newUsername").value;
     const newPassword = document.getElementById("newPassword").value;
-    
+
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const existingUser = users.find(user => user.username === newUsername);
+    if (existingUser) {
+        alert("Username already exists. Please choose a different username.");
+        return;
+    }
+
+    users.push({ username: newUsername, password: newPassword });
+    localStorage.setItem("users", JSON.stringify(users));
+
     alert("User registered successfully. Redirecting to login page.");
     window.location.href = "login.html";
 }
