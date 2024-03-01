9be9925c2b4520842829ad2eaf3a21347f18fe8f
sumedhaKl <sumedhakamaraju@gmail.com>
2024-02-29 16:57:51 -0800

    first commit

---
+++ _posts/2024-02-26-Blog_about_CPT_Project.md
@@ -0,0 +1,11 @@
+---
+toc: true
+comments: true
+layout: post
+title: CPT Project
+description: Explanation of CPT project structure and overview
+type: tangibles
+courses: { compsci: {week: 8} }
+---
+## CPT Project
+
--- _posts/signup.js
+++ _posts/signup.js
@@ -4,8 +4,14 @@ function signup(event) {
     const newUsername = document.getElementById("newUsername").value;
     const newPassword = document.getElementById("newPassword").value;
 
+    console.log("New username:", newUsername);
+    console.log("New password:", newPassword); 
+
     const users = JSON.parse(localStorage.getItem("users")) || [];
+    console.log("Existing users:", users);
     const existingUser = users.find(user => user.username === newUsername);
+    console.log("Existing user:", existingUser);
+
     if (existingUser) {
         alert("Username already exists. Please choose a different username.");
         return;
